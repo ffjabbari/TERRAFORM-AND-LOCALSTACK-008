f23483b6857793981f51b69687d2561e6bc23f47
Fred <fjabbari@Freds-MacBook-Pro.local>
2024-04-08 20:19:51 -0500

    aback to aa

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state"
+    key    = "terraform.tfstate"
+  }
+
+  required_version = "= 1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+}
